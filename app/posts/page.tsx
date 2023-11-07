640b75a050e2a057ad33b04f82d3608be199ee1b
utsav82 <utsa.yadav.94@gmail.com>
2023-11-07 16:45:56 +0530

    resize

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -6,7 +6,7 @@ export const metadata = {
 };
 export default function Blog() {
   return (
-    <div className="container max-w-4xl py-6 lg:py-10">
+    <div className="container max-w-4xl py-6 lg:py-10 mx-auto">
       <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
         <div className="flex-1 space-y-4">
           <h1 className="inline-block font-heading text-4xl tracking-tight lg:text-5xl">
